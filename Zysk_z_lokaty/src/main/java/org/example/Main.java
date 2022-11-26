e559783db81aa71036ba9dfbe15d04d685342d8c
Filip-Politowski <fil.politowski@gmail.com>
2022-11-26 14:45:50 +0100

    Cwiczenie - obliczanie zysku z lokaty

--- Zysk_z_lokaty/src/main/java/org/example/Main.java
+++ Zysk_z_lokaty/src/main/java/org/example/Main.java
@@ -4,5 +4,31 @@ public class Main {
     public static void main(String[] args) {
         float deposit = 1000.00f;
         float numDays = 180;
+        float interestRate = 0.06f;
+        final float daysInYear = 365;
+        float tax = 0.19f;
+
+
+        /*
+         6000 (wpłacona kwota) x 180 (półroczny okres lokaty)
+         x 0,03 (oprocentowanie lokaty) / 365 (liczba dni w roku) = 88,77 zł
+
+            wzór na obliczenie zysku z lokaty
+            odsetki = depozyt x ilośćDni * oprocentowanie / ilośćDniWRoku
+
+            podatek = odsetki * podatek;
+
+            zysk = odsetki - podatek;
+         */
+        float interest = deposit * numDays * interestRate / daysInYear;
+        System.out.println("Przychód z lokaty: " + interest);
+
+        float taxValue = interest * tax;
+        System.out.println("Podatek: " + taxValue);
+
+        float incomeAfterTax = interest - taxValue;
+        System.out.println("Zysk po podatku: " + incomeAfterTax);
+
     }
+
 }
